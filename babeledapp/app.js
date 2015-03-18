f494e54b3d50ff06a0402406498b31bffb3f8ec5
Tarrence van As <tarrence@box.com>
2015-03-17 20:04:00 -0700

    Removed built code

--- babeledapp/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var React = require("react");
-var Immutable = require("immutable");
-var SheetApp = require("./components/SheetApp.js");
-
-var initialData = Immutable.fromJS(JSON.parse(document.getElementById("initial-state").innerHTML));
-
-React.render(React.createElement(SheetApp, { sheetData: initialData }), document.getElementById("app-main"));
